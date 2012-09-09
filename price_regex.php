aeba496a0a2c88794195b746c3ce36f5dc98553d
AnuVi <anuviks@gmail.com>
2012-09-09 19:06:35 +0300

    new

---
+++ price_regex.php
@@ -0,0 +1,23 @@
+<?php
+// grouped by 3 if the price has more than 4 characters
+
+//hindades grupeeriks 3-kaupa kui on vähemalt viiekohaline arv
+
+
+   function price_space($string1) {
+      
+   	$s=strlen($string1);
+   	if ($s>4)
+   	{
+  		return preg_replace('/(?=(([0-9]{3})*)$)/',' ', $string1);
+   	}        
+    	else 
+   	{
+     		return $string1;
+   	}
+   }  
+
+
+
+
+?>
